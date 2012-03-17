f0208ea23a597b9ed0126d05f14980e5a93d0a78
brandonjp <brandonjp@gmail.com>
2012-03-16 22:35:58 -0400

    display 'no current events' if none exist

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,5 @@
 class HomeController < ApplicationController
   def index
     @event = Event.all(sort: [[ :date, :asc ]], :conditions => {:date => Date.today.to_datetime..(Date.today+24).to_datetime}).first
-    @users = User.all
   end
 end
